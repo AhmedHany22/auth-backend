4b5ee25a1086383cbff0a186a0cfb3088415ca5b
Ahmed Hany <ahmed_hany19@outlook.com>
2023-03-27 15:00:23 +0200

    Add New Origin for "CORS"

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,9 @@ const allowedOrigins = [
   "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://127.0.0.1:8000",
-  "http://localhost:3500",
   "http://localhost:3000",
+  "http://localhost:3500",
+  "http://localhost:8000",
 ];
 
 module.exports = allowedOrigins;
